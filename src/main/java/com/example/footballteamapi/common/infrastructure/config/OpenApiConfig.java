2dac5117a38d98684676a5d62aaa20bcc44271fa
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2025-03-10 09:58:07 +0300

    Task 5 : Define common package based on hexagonal architecture

---
+++ src/main/java/com/example/footballteamapi/common/infrastructure/config/OpenApiConfig.java
@@ -0,0 +1,32 @@
+package com.example.footballteamapi.common.infrastructure.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "Sercan Noyan Germiyanoğlu",
+                        url = "https://github.com/Rapter1990/footballteamapi"
+                ),
+                description = "Case Study - Football Team Api" +
+                        " (Java 21, Spring Boot, Postgres, JUnit, Spring Security, JWT, Docker, Kubernetes, Prometheus, Grafana, Github Actions (CI/CD) ) ",
+                title = "footballteamapi",
+                version = "1.0.0"
+        )
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT Token",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfig {
+
+}
