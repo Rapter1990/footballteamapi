754193a180313e58bcfe82fae184cbdc74f4e715
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2025-03-10 08:48:27 +0300

    Task 1 : Initial commit

---
+++ src/main/java/com/example/footballteamapi/FootballTeamApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.footballteamapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballTeamApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballTeamApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/footballteamapi/FootballTeamApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.footballteamapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballTeamApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
