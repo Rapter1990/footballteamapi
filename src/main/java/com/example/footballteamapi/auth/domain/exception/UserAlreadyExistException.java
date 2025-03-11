e3410384f85d9f615af4fd37944213a515c5fe7d
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2025-03-11 15:53:36 +0300

    Task 12 : Revise UserAlreadyExistException

--- src/main/java/com/example/footballteamapi/auth/domain/exception/UserAlreadyExistException.java
+++ src/main/java/com/example/footballteamapi/auth/domain/exception/UserAlreadyExistException.java
@@ -1,11 +1,15 @@
 package com.example.footballteamapi.auth.domain.exception;
 
+import org.springframework.http.HttpStatus;
+
 import java.io.Serial;
 
 public class UserAlreadyExistException extends RuntimeException {
 
     @Serial
     private static final long serialVersionUID = 3919438143252770610L;
+
+    public static final HttpStatus STATUS = HttpStatus.CONFLICT;
 
     private static final String DEFAULT_MESSAGE = """
             User already exist!
